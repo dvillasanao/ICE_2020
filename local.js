f11946c4f9292d0b96ede19b0e46d406f17ee95b
dvillasanao <dvillasanao@hotmail.com>
2023-11-16 13:57:29 -0600

    ICE versión incompleta

---
+++ local.js
@@ -0,0 +1,18 @@
+<script>
+    $(function() {
+        $("#toc h2").html("Contenido");
+    });
+</script>
+<script>
+    $(function() {
+        $(".sidebar h2").html("Contenido");
+    });
+</script>
+
+<script>
+  $(document).ready(function() {
+    $head = $('#header');
+    $head.prepend('<div class="knitr source"><img src="images/conapo.png" width="220px" align="right"   ></div>')
+  });
+</script>
+
